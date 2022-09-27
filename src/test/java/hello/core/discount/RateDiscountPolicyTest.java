10bf8c1106ed7ec42071287f456db698a4964a4b
lynjun97@naver.com <lynjun97@naver.com>
2022-09-27 13:28:42 +0900

    스프링 핵심 원리 - 기본편

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -18,9 +18,9 @@ class RateDiscountPolicyTest {
         //given
         Member member = new Member(1L, "memberVIP", Grade.VIP);
         //when
-        int discount = discountPolicy.discount(member, 10000);
+        int discount = discountPolicy.discount(member, 20000);
         //then
-        assertThat(discount).isEqualTo(1000);
+        assertThat(discount).isEqualTo(2000);
     }
 
     @Test
